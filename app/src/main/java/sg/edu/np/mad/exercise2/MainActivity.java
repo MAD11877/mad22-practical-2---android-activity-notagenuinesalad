e7e21e6eff5f7c9aa881748ed9dc5b032434e73f
Nur Harith Mirza Norrahamad <s10207999@connect.np.edu.sg>
2022-07-06 21:06:55 +0800

    Practical 2 - Updated

--- app/src/main/java/sg/edu/np/mad/exercise2/MainActivity.java
+++ app/src/main/java/sg/edu/np/mad/exercise2/MainActivity.java
@@ -8,16 +8,30 @@ import android.view.View;
 import android.widget.Toast;
 import android.widget.Toolbar;
 import android.view.Menu;
+import android.widget.Button;
+
 
 public class MainActivity extends AppCompatActivity {
 
-
-    public void onDefaultToggleClick(View view) {
-        Toast.makeText(this, "DefaultToggle", Toast.LENGTH_SHORT).show();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
     }
 
-    public void onCustomToggleClick(View view) {
-        Toast.makeText(this, "CustomToggle", Toast.LENGTH_SHORT).show();
+    User user = new User("name", "description", 10, true);
+
+    public void followButton(View v){
+        Button follow = findViewById(R.id.button);
+        if(!user.followed){
+            follow.setText("Unfollow");
+            user.followed = true;
+        }
+        else{
+            follow.setText("Follow");
+            user.followed = false;
+        }
+
     }
 
 }
